7b2ffb8fb79dd0a9f8a153cc47736b546873c831
Rocky Feng <folowing@gmail.com>
2021-09-12 18:58:52 +0800

    move BrowserFs logic to main.js

--- apps/remix-ide/src/main.js
+++ apps/remix-ide/src/main.js
@@ -1,1 +1,10 @@
-import './index'
+window.onload = () => {
+  BrowserFS.install(window)
+  BrowserFS.configure({
+    fs: 'LocalStorage'
+  }, function (e) {
+    if (e) console.log(e)
+    window.remixFileSystem = BrowserFS.BFSRequire('fs')
+    require('./index')
+  })
+}
